4c67ded4761435a259c860bb09115aebba651d70
MrJohn6774 <45367030+MrJohn6774@users.noreply.github.com>
2020-03-14 05:51:07 +0800

    delete main class

--- flightcontrol.py
+++ flightcontrol.py
@@ -3,29 +3,28 @@ import time
 import Servo
 
 
-class main:
-
-    aileron_left = 11
-    aileron_right = 12
-    elevator = 15
-    rudder = 16
-    chan_list = [aileron_left, aileron_right, elevator, rudder]
-
-    def initialize():
-        global chan_list
-        gpio.setmode(gpio.BOARD)
-        gpio.setup(chan_list, gpio.OUT)
-
-    @staticmethod
-    def test():
-        main.initialize()
-        global aileron_left
-        al = Servo(aileron_left)
-        al.pwm.start(7.5)
-        print("Left Aileron Down")
-        time.sleep(0.2)
-        al.pwm.ChangeDutyCycle(9.5)
-        print("Left Aileron Up")
+aileron_left = 11
+aileron_right = 12
+elevator = 15
+rudder = 16
+chan_list = [aileron_left, aileron_right, elevator, rudder]
 
 
-main.test()
+def initialize():
+    global chan_list
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(chan_list, gpio.OUT)
+
+
+def test():
+    initialize()
+    global aileron_left
+    al = Servo(aileron_left)
+    al.pwm.start(7.5)
+    print("Left Aileron Down")
+    time.sleep(0.2)
+    al.pwm.ChangeDutyCycle(9.5)
+    print("Left Aileron Up")
+
+
+test()

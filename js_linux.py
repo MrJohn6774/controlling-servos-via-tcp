a2a36170857d28527224d555911d0cff7f7e3eaa
MrJohn6774 <45367030+MrJohn6774@users.noreply.github.com>
2020-03-14 10:58:25 +0800

    Update js_linux.py

--- js_linux.py
+++ js_linux.py
@@ -10,7 +10,7 @@ print('Available devices:')
 
 for fn in os.listdir('/dev/input'):
     if fn.startswith('js'):
-        print('  /dev/input/%s' % (fn))
+        print(('  /dev/input/%s' % (fn)))
 
 # We'll store the states here.
 axis_states = {}
@@ -93,28 +93,28 @@ button_map = []
 
 # Open the joystick device.
 fn = '/dev/input/js0'
-print('Opening %s...' % fn)
+print(('Opening %s...' % fn))
 jsdev = open(fn, 'rb')
 
 # Get the device name.
-#buf = bytearray(63)
+# buf = bytearray(63)
 buf = array.array('B', [0] * 64)
-ioctl(jsdev, 0x80006a13 + (0x10000 * len(buf)), buf) # JSIOCGNAME(len)
-js_name = buf.tobytes().rstrip(b'\x00').decode('utf-8')
-print('Device name: %s' % js_name)
+ioctl(jsdev, 0x80006a13 + (0x10000 * len(buf)), buf)  # JSIOCGNAME(len)
+js_name = buf.tostring().rstrip(b'\x00').decode('utf-8')
+print(('Device name: %s' % js_name))
 
 # Get number of axes and buttons.
 buf = array.array('B', [0])
-ioctl(jsdev, 0x80016a11, buf) # JSIOCGAXES
+ioctl(jsdev, 0x80016a11, buf)  # JSIOCGAXES
 num_axes = buf[0]
 
 buf = array.array('B', [0])
-ioctl(jsdev, 0x80016a12, buf) # JSIOCGBUTTONS
+ioctl(jsdev, 0x80016a12, buf)  # JSIOCGBUTTONS
 num_buttons = buf[0]
 
 # Get the axis map.
 buf = array.array('B', [0] * 0x40)
-ioctl(jsdev, 0x80406a32, buf) # JSIOCGAXMAP
+ioctl(jsdev, 0x80406a32, buf)  # JSIOCGAXMAP
 
 for axis in buf[:num_axes]:
     axis_name = axis_names.get(axis, 'unknown(0x%02x)' % axis)
@@ -123,15 +123,15 @@ for axis in buf[:num_axes]:
 
 # Get the button map.
 buf = array.array('H', [0] * 200)
-ioctl(jsdev, 0x80406a34, buf) # JSIOCGBTNMAP
+ioctl(jsdev, 0x80406a34, buf)  # JSIOCGBTNMAP
 
 for btn in buf[:num_buttons]:
     btn_name = button_names.get(btn, 'unknown(0x%03x)' % btn)
     button_map.append(btn_name)
     button_states[btn_name] = 0
 
-print('%d axes found: %s' % (num_axes, ', '.join(axis_map)))
-print('%d buttons found: %s' % (num_buttons, ', '.join(button_map)))
+print(('%d axes found: %s' % (num_axes, ', '.join(axis_map))))
+print(('%d buttons found: %s' % (num_buttons, ', '.join(button_map))))
 
 # Main event loop
 while True:
@@ -140,20 +140,20 @@ while True:
         time, value, type, number = struct.unpack('IhBB', evbuf)
 
         if type & 0x80:
-             print("(initial)", end="")
+            print("(initial)")
 
         if type & 0x01:
             button = button_map[number]
             if button:
                 button_states[button] = value
                 if value:
-                    print("%s pressed" % (button))
+                    print(("%s pressed" % (button)))
                 else:
-                    print("%s released" % (button))
+                    print(("%s released" % (button)))
 
         if type & 0x02:
             axis = axis_map[number]
             if axis:
                 fvalue = value / 32767.0
                 axis_states[axis] = fvalue
-                print("%s: %.3f" % (axis, fvalue))
+                print(("%s: %.3f" % (axis, fvalue)))
